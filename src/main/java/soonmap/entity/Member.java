d5087a33a751e1e07974a8c939a4ba15f7623037
List <61047602+namest504@users.noreply.github.com>
2023-07-15 02:43:52 +0900

    feat: 유저 Entity

---
+++ src/main/java/soonmap/entity/Member.java
@@ -0,0 +1,60 @@
+package soonmap.entity;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Getter
+public class Member implements UserDetails {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String userEmail;
+
+    private String userName;
+
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
