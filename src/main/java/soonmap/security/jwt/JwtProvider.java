e984a2cf6ebf57566e0b4fadeeaa205e531d9a16
List <61047602+namest504@users.noreply.github.com>
2023-07-18 15:22:15 +0900

    revert: 토큰 발급 로직 분리

--- src/main/java/soonmap/security/jwt/JwtProvider.java
+++ src/main/java/soonmap/security/jwt/JwtProvider.java
@@ -3,7 +3,6 @@ package soonmap.security.jwt;
 import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
@@ -20,9 +19,12 @@ public class JwtProvider {
     @Value("${JWT_SECRET_KET}")
     private String JWT_SECRET_KET;
 
+    private final Long accessTokenValidTime = 1000 * 60L * 5L; // 5분
+    private final Long refreshTokenValidTime = 1000 * 60 * 60 * 24 * 7L; // 1주
+
     private final MemberRepository memberRepository;
 
-    public String createToken(String email, String TokenType, Long Token_Expire_Time) {
+    public String createAccessToken(String email) {
         Claims claims = Jwts.claims().setSubject("access_token");
         claims.put("userEmail", email);
         Date currentTime = new Date();
@@ -30,24 +32,50 @@ public class JwtProvider {
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(currentTime)
-                .setExpiration(new Date(currentTime.getTime() + Token_Expire_Time))
+                .setExpiration(new Date(currentTime.getTime() + accessTokenValidTime))
                 .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KET)
                 .compact();
     }
 
+    public String createRefreshToken(String email) {
+        Claims claims = Jwts.claims().setSubject("refresh_token");
+        claims.put("userEmail", email);
+        Date currentTime = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(currentTime)
+                .setExpiration(new Date(currentTime.getTime() + refreshTokenValidTime))
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KET)
+                .compact();
+    }
+
+//    public String createToken(String email, String TokenType, Long Token_Expire_Time) {
+//        Claims claims = Jwts.claims().setSubject("access_token");
+//        claims.put("userEmail", email);
+//        Date currentTime = new Date();
+//
+//        return Jwts.builder()
+//                .setClaims(claims)
+//                .setIssuedAt(currentTime)
+//                .setExpiration(new Date(currentTime.getTime() + Token_Expire_Time))
+//                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KET)
+//                .compact();
+//    }
+
     public UsernamePasswordAuthenticationToken getAuthentication(String token) {
-        Long uid = getUidFromToken(token);
-        Member member = memberRepository.findById(uid)
+        String Email = getEmailFromToken(token);
+        Member member = memberRepository.findMemberByUserEmail(Email)
                 .orElseThrow(() -> new RuntimeException("Member 를 찾지 못했습니다."));
         MemberPrincipal memberPrincipal = new MemberPrincipal(member);
         return new UsernamePasswordAuthenticationToken(memberPrincipal, token,
                 member.getAuthorities());
     }
 
-    public Long getUidFromToken(String token) {
+    public String getEmailFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(JWT_SECRET_KET).build().parseClaimsJws(token).getBody()
-                .get("uid",
-                        Long.class);
+                .get("userEmail",
+                        String.class);
     }
 
     public Boolean validateAccessToken(String token) {
