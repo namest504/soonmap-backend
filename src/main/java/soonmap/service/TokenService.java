b24cc591cb203f6b8c0fec49d4df708e50d78c97
List <61047602+namest504@users.noreply.github.com>
2023-07-18 15:22:51 +0900

    refactor: 코드 리팩토링

--- src/main/java/soonmap/service/TokenService.java
+++ src/main/java/soonmap/service/TokenService.java
@@ -10,19 +10,21 @@ import soonmap.security.jwt.JwtProvider;
 @Service
 @RequiredArgsConstructor
 public class TokenService {
+
     private final Long AccessexpireTimeMs = 300000l; // 5분
     private final Long RefreshExpireTimeMs = 1000 * 60 * 60 * 60L; // 3시간
+
     private final JwtProvider jwtProvider;
 
-    public TokenDto createTokens(String userEmail) {
-        TokenDto tokenDto = new TokenDto();
-        String accessToken = jwtProvider.createToken(userEmail, TokenType.ACCESS_TOKEN.getValue(), AccessexpireTimeMs);
-        String refreshToken = jwtProvider.createToken(userEmail, TokenType.REFRESH_TOKEN.getValue(), RefreshExpireTimeMs);
-
-
-        tokenDto.setAccessToken(accessToken);
-        tokenDto.setRefreshToken(refreshToken);
-
-        return tokenDto;
-    }
+//    public TokenDto createTokens(String userEmail) {
+//        TokenDto tokenDto = new TokenDto();
+//        String accessToken = jwtProvider.createToken(userEmail, TokenType.ACCESS_TOKEN.getValue(), AccessexpireTimeMs);
+//        String refreshToken = jwtProvider.createToken(userEmail, TokenType.REFRESH_TOKEN.getValue(), RefreshExpireTimeMs);
+//
+//
+//        tokenDto.setAccessToken(accessToken);
+//        tokenDto.setRefreshToken(refreshToken);
+//
+//        return tokenDto;
+//    }
 }
