daf5c132800feb58ff5ce531424bb0ea4b49f239
List <61047602+namest504@users.noreply.github.com>
2023-07-28 19:45:45 +0900

    feat: 게시글 조회 추가
    전체 게시글 조회
    본인 게시글 조회

--- src/main/java/soonmap/repository/ArticleRepository.java
+++ src/main/java/soonmap/repository/ArticleRepository.java
@@ -1,9 +1,15 @@
 package soonmap.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soonmap.entity.Article;
+import soonmap.entity.Member;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    Page<Article> findAll(Pageable pageable);
+    Page<Article> findAllByMember(Member member, Pageable pageable);
 }
