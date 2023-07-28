ca875a113bf550309123309a0f4dd72caa7f7e57
List <61047602+namest504@users.noreply.github.com>
2023-07-29 03:47:06 +0900

    chore: CORS 설정 추가
    Origin
    Header
    Credential

--- src/main/java/soonmap/config/WebConfig.java
+++ src/main/java/soonmap/config/WebConfig.java
@@ -10,6 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("/**")
+                .allowedHeaders("*")
+                .allowCredentials(true)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
