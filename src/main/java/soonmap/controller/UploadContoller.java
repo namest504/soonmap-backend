1297c150f68c15c1e88ff9e18f25459e08148b5f
List <61047602+namest504@users.noreply.github.com>
2023-07-28 17:58:48 +0900

    feat: 업로드 관련 API 추가
    이미지 및 파일 업로드 & 링크 반환

---
+++ src/main/java/soonmap/controller/UploadContoller.java
@@ -0,0 +1,41 @@
+package soonmap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import soonmap.service.S3Service;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/upload")
+public class UploadContoller {
+
+    private final S3Service s3Service;
+
+    @Value("${CLOUD_FRONT_URL}")
+    private String CLOUD_FRONT_URL;
+
+    @Secured({"ROLE_ADMIN", "ROLE_MANAGER", "ROLE_STAFF"})
+    @PostMapping("/image")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile image) throws IOException {
+        String upload = s3Service.upload(image, "/images");
+        return ResponseEntity.ok()
+                .body(CLOUD_FRONT_URL + upload);
+    }
+
+    @Secured({"ROLE_ADMIN", "ROLE_MANAGER", "ROLE_STAFF"})
+    @PostMapping("/file")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        String upload = s3Service.upload(file, "/files");
+        return ResponseEntity.ok()
+                .body(CLOUD_FRONT_URL + upload);
+    }
+}
