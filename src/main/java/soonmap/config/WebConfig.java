ea2d9383c2d348edbd3a4c9cd720ce86b3fe0224
List <61047602+namest504@users.noreply.github.com>
2023-07-30 20:48:11 +0900

    fix: CORS

--- src/main/java/soonmap/config/WebConfig.java
+++ src/main/java/soonmap/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+//                .allowCredentials(true)
                 .exposedHeaders("Access-Token")
                 .allowedMethods(
                         HttpMethod.GET.name(),
