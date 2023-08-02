231e2ad01c821102ce737d6b991f2556b845b9e4
hong <kevin0928@naver.com>
2023-08-03 01:49:47 +0900

    Fix: fix entity mapping

--- src/main/java/soonmap/entity/Floor.java
+++ src/main/java/soonmap/entity/Floor.java
@@ -16,7 +16,4 @@ public class Floor {
     private Long id;
     private String description; // 층별 설명
     private String dir; // S3 이미지 경로
-    @ManyToOne
-    @JoinColumn(name = "building_id")
-    private Building building;
 }
