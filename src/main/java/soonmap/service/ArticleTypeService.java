fd8fd1de46cf46d65a90965a68245a501872c57c
lim1t <namest504@gmail.com>
2023-08-06 04:06:21 +0900

    Dev/lim1t (#43)
    * chore: 환경 설정
    
    project 이름 soonmap으로 변경
    build 파일 ignore 처리
    
    * test: 로직 변경 수정
    
    * test: 로직 변경 수정
    
    * feat: Admin API 추가
    
    공지사항 페이지 조회
    게시글 카테고리 페이지 조회
    게시글 수정
    게시글 삭제
    건물 정보 업로드
    건물 정보 수정
    건물 정보 삭제
    건물 정보 페이지 조회
    도면 조회
    도면 업로드
    도면 수정
    도면 삭제
    
    * feat: DTO 추가
    
    * feat: 엔티티 수정
    
    * feat: Repository 기능 추가
    
    * feat: Service 기능 추가
    
    * fix: 로직 수정
    
    page 반환값 수정
    카테고리명 중복 검증

--- src/main/java/soonmap/service/ArticleTypeService.java
+++ src/main/java/soonmap/service/ArticleTypeService.java
@@ -5,11 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import soonmap.entity.Article;
 import soonmap.entity.ArticleType;
 import soonmap.exception.CustomException;
 import soonmap.repository.ArticleTypeRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -38,5 +40,9 @@ public class ArticleTypeService {
     public Long deleteById(Long id) {
         articleTypeRepository.deleteById(id);
         return id;
+    }
+
+    public Optional<ArticleType> findByTypeName(String typeName) {
+        return articleTypeRepository.findByTypeName(typeName);
     }
 }
