c28f24c6cf04eb1287f94e2d04cc92ce41a58f1b
List <61047602+namest504@users.noreply.github.com>
2023-08-06 00:19:22 +0900

    feat: Service 기능 추가

--- src/main/java/soonmap/service/BuildingInfoService.java
+++ src/main/java/soonmap/service/BuildingInfoService.java
@@ -1,18 +1,16 @@
 package soonmap.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import soonmap.dto.BuildingInfoDto;
 import soonmap.entity.Building;
 import soonmap.entity.Floor;
 import soonmap.repository.BuildingRepository;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static soonmap.dto.BuildingInfoDto.*;
@@ -40,7 +38,25 @@ public class BuildingInfoService {
     public List<BuildingResponseDto> getBuildingByName(String name) {
         List<Building> buildings = buildingRepository.findBuildingsByName(name);
 
-            List<BuildingResponseDto> buildingResponseDtos = buildings.stream().map(BuildingResponseDto::new).collect(Collectors.toList());
-            return buildingResponseDtos;
-        }
+        List<BuildingResponseDto> buildingResponseDtos = buildings.stream().map(BuildingResponseDto::new).collect(Collectors.toList());
+        return buildingResponseDtos;
+    }
+
+    public Building save(Building building) {
+        return buildingRepository.save(building);
+    }
+
+    public Long deleteById(Long id) {
+        buildingRepository.deleteById(id);
+        return id;
+    }
+
+    public Page<Building> findPageAll(int page, int length) {
+        PageRequest pageRequest = PageRequest.of(page, length);
+        return buildingRepository.findAll(pageRequest);
+    }
+
+    public Optional<Building> findOneById(Long buildingId) {
+        return buildingRepository.findById(buildingId);
+    }
 }
