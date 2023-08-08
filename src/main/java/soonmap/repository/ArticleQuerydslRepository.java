bd8267ef4cc93ed05407868abd6f2244e4624ee4
List <61047602+namest504@users.noreply.github.com>
2023-08-09 07:25:58 +0900

    feat: 게시글 검색 로직 추가

---
+++ src/main/java/soonmap/repository/ArticleQuerydslRepository.java
@@ -0,0 +1,46 @@
+package soonmap.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import soonmap.entity.Article;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static soonmap.entity.QArticle.article;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleQuerydslRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public Page<Article> findArticlesByCondition(String typeName, LocalDateTime startDate, LocalDateTime endDate, String title, Pageable pageable) {
+
+        List<Article> articleList = queryFactory
+                .selectFrom(article)
+                .where(typeNameEq(typeName), dateBetween(startDate, endDate), titleContains(title))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(articleList, pageable, articleList.size());
+    }
+
+    private BooleanExpression typeNameEq(String typeName) {
+        return typeName != null ? article.articleType.typeName.eq(typeName) : null;
+    }
+
+    private BooleanExpression dateBetween(LocalDateTime startDate, LocalDateTime endDate) {
+        return (startDate != null && endDate != null) ? article.createAt.between(startDate, endDate) : null;
+    }
+
+    private BooleanExpression titleContains(String title) {
+        return title != null ? article.title.contains(title) : null;
+    }
+}
