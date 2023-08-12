6ca6855c601b36d53e96e0502697cd8dfbbdaf05
hong <kevin0928@naver.com>
2023-08-12 16:38:05 +0900

    feat: lunch Recommend init

--- src/main/java/soonmap/entity/Menu.java
+++ src/main/java/soonmap/entity/Menu.java
@@ -1,2 +1,21 @@
-package soonmap.entity;public class Menu {
+package soonmap.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long
 }
--- src/main/java/soonmap/repository/MenuRepository.java
+++ src/main/java/soonmap/repository/MenuRepository.java
@@ -1,2 +1,8 @@
-package soonmap.repository;public interface MenuRepository {
+package soonmap.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import soonmap.entity.Menu;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
 }
