522514cc57206867650a7fee331fa87abf32eb1a
List <61047602+namest504@users.noreply.github.com>
2023-08-16 10:53:14 +0900

    feat: 본인 공지사항 조회 추가

--- src/main/java/soonmap/repository/NoticeQueryDslRepository.java
+++ src/main/java/soonmap/repository/NoticeQueryDslRepository.java
@@ -4,14 +4,17 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import soonmap.entity.Article;
 import soonmap.entity.Notice;
 
 import java.util.List;
 
+import static soonmap.entity.QArticle.article;
 import static soonmap.entity.QNotice.notice;
 
 @Repository
@@ -71,5 +74,21 @@ public class NoticeQueryDslRepository {
 
     private BooleanExpression titleContains(String title) {
         return title != null ? notice.title.contains(title) : null;
+    }
+
+    public Page<Notice> findAllByMemberId(Long memberId, Pageable pageable) {
+        List<Notice> noticeList = queryFactory
+                .selectFrom(notice)
+                .where(notice.member.id.eq(memberId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long countNotice = queryFactory.select(notice.count())
+                .from(notice)
+                .where(notice.member.id.eq(memberId))
+                .fetchOne();
+
+        return new PageImpl<>(noticeList, pageable, countNotice);
     }
 }
